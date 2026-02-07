e0f8ecab886907a0d03fa710981d37616cce3bca
colin zeng <89177519@qq.com>
2026-02-07 16:36:48 +0800

    Update blog.sh

--- blog.sh
+++ blog.sh
@@ -8,8 +8,16 @@ if test "$1" = "run"; then
   bundle exec jekyll serve --watch --host=0.0.0.0 --port=$port
 elif test "$1" = "build"; then
   bundle exec jekyll build --destination=dist
+  # copy favicon to site root to improve compatibility (/favicon.ico)
+  if [ -f static/img/favicon.ico ]; then
+    cp static/img/favicon.ico dist/favicon.ico || true
+  fi
 elif test "$1" = "deploy"; then
   bundle exec jekyll build --destination=dist
+  # copy favicon to site root to improve compatibility (/favicon.ico)
+  if [ -f static/img/favicon.ico ]; then
+    cp static/img/favicon.ico dist/favicon.ico || true
+  fi
   cos-upload local:./dist blog:/
   curl -fL -u freshCDN "https://cloud.page404.cn/api/fresh-cdn/"
 else
